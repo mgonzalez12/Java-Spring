6cb6d251a2f8ec840fbfc35b4e91d83ad3d1ddc3
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2018-08-13 06:21:35 -0500

    inyeccion por constructor

--- src/main/java/com/proyecto/spring/App.java
+++ src/main/java/com/proyecto/spring/App.java
@@ -1,37 +1,21 @@
 package com.proyecto.spring;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.proyecto.beans.AppConfig;
-import com.proyecto.beans.AppConfig2;
-import com.proyecto.beans.Mundo;
+import com.proyecto.beans.Persona;
+
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		//ApplicationContext appContext = new ClassPathXmlApplicationContext("com/proyecto/xml/beans.xml");
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/proyecto/xml/beans.xml");
+		Persona per = (Persona) appContext.getBean("persona");
 		
-		// **  una manera de llamar los AppConfig **
-		//ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class,AppConfig2.class);
-		AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
-		appContext.register(AppConfig.class);
-		appContext.register(AppConfig2.class);
-		appContext.refresh();
+		// video 6 mito 3:15
 		
-		
-		// usando el patron Factory
-		Mundo m = (Mundo) appContext.getBean("marte");
-		
-		System.out.println(m.getSaludo());
-		
-		
-		
-		
-		
-		
+		System.out.println(per.getId()+ " "+per.getNombre()+" "+per.getApodo());
 	}
 
 }
