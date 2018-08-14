0c0fd4450363bb8143cae578899b64838f1ef19e
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2018-08-13 22:43:17 -0500

    Bean Scope en Spring

--- src/main/java/com/proyecto/spring/App.java
+++ src/main/java/com/proyecto/spring/App.java
@@ -10,10 +10,14 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/proyecto/xml/beans.xml");
 		Persona per = (Persona) appContext.getBean("persona");
-		
-		
+		Persona per2 = (Persona) appContext.getBean("persona");
 		
 		System.out.println(per.getId()+" "+ per.getNombre()+" "+ per.getApodo()+" "+per.getPais().getNombre()+" "+per.getCiudad().getNombre());
+		System.out.println(per.getId()+" "+ per2.getNombre()+" "+ per2.getApodo()+" "+per2.getPais().getNombre()+" "+per2.getCiudad().getNombre());
+	
+			// los Scope beans singleton es la mista instancia - prototype son objetos diferentes
+		System.out.println(per);
+		System.out.println(per2);
 	}
 
 }
